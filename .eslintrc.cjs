eb08d5eaf89d7c940f373ab8cf440b1966c91ee7
Noob-H4cker <alsamiul123@gmail.com>
2023-12-30 22:19:21 +0600

    navbar added

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,29 +12,12 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
-    // React Rules
-    "react/prop-types": "error",
-    "react/jsx-props-no-spreading": "off",
-    "react/no-unescaped-entities": "off",
-    "react/no-unused-state": "error",
-    "react/jsx-filename-extension": 0,
-
-    // React Hooks Rules
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
-
-    // JSX Accessibility Rules
-    "jsx-a11y/anchor-is-valid": "off",
-
-    // Import Rules
-    "import/react": 0,
-
-    // Additional Stylistic Rules
-    indent: ["error", 2],
-    quotes: ["error", "single"],
-    // Add other style rules as needed
-
-    // Avoiding console.logs in production
-    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-var": "error",
+    camelcase: "error",
+    "no-empty": "error",
+    "default-case": "error",
+    eqeqeq: "error",
+    "max-lines": ["error", 50],
+    "max-depth": ["error", 3],
   },
 };
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": "http://localhost:4000",
+    },
+  },
+  resolve: {
+    extensions: [
+      ".js",
+      ".jsx",
+      ".ts",
+      ".tsx",
+      ".json",
+      ".css",
+      ".scss",
+      ".less",
+      ".png",
+      ".jpg",
+      ".gif",
+      ".svg",
+      ".mp4",
+      ".webm",
+      ".woff",
+      ".woff2",
+    ],
+  },
+});
