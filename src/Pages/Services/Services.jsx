5108b0033e5dfb54f559d7faa7b88ec749571535
Noob-H4cker <alsamiul123@gmail.com>
2024-01-01 22:27:14 +0600

    service component responsive

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import { useQuery } from "@tanstack/react-query";
 import ServicesItem from "./ServicesItem";
 
@@ -23,10 +24,17 @@ const Services = () => {
     return <span> Error : {error.message}</span>;
   }
   return (
-    <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+    <div className="mt-10 ">
+        <div className="text-center lg:px-40">
+            <h3 className="text-xl text-red-500">Service</h3>
+            <h1 className="text-3xl font-bold">Our Service Area</h1>
+            <p className="mt-3">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+        </div>
+        <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 px-5">
       {services.map((service, i) => (
         <ServicesItem key={i} service={service} />
       ))}
+    </div>
     </div>
   );
 };
