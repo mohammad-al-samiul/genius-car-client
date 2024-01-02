9e1263ae7da0e6fbffd9b144200e92208b72cea0
Noob-H4cker <alsamiul123@gmail.com>
2024-01-02 17:53:08 +0600

    login page design completed

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,3 +1,5 @@
+import { FaFacebook, FaGoogle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import loginImage from "../../assets/images/login/login";
 const Login = () => {
   return (
@@ -8,7 +10,7 @@ const Login = () => {
         </div>
         <div className="card w-full max-w-sm border border-slate-300 py-6">
           <h1 className="text-center text-2xl font-bold">Login</h1>
-          <form className="card-body">
+          <form className="pb-0 card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
@@ -44,6 +46,32 @@ const Login = () => {
               />
             </div>
           </form>
+          <label className="text-center my-3">
+            or Sign in with
+            <br />
+          </label>
+          <div className="flex justify-center gap-x-3 mb-3 text-red-400">
+            <span>
+              <FaFacebook className="text-2xl cursor-pointer" />
+            </span>
+            <span>
+              <FaGoogle className="text-2xl cursor-pointer" />
+            </span>
+          </div>
+
+          <div>
+            <label>
+              <span className="block text-center">
+                Create an account?
+                <Link
+                  to={"/signup"}
+                  className="link link-hover text-red-500 font-bold"
+                >
+                  Signup
+                </Link>
+              </span>
+            </label>
+          </div>
         </div>
       </div>
     </div>
