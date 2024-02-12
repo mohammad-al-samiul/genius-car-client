e0a4b2ce062ec2ffb07721872cf3f12558ff86de
Noob-H4cker <alsamiul123@gmail.com>
2024-02-12 20:38:38 +0600

    blog section completed

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,14 +1,75 @@
 /* eslint-disable react/no-unescaped-entities */
+import wrenchImg from "../../assets/icons/Wrench";
+import checkImg from "../../assets/icons/check";
+import deliveryImg from "../../assets/icons/deliveryt";
+import groupImg from "../../assets/icons/group";
+import personImg from "../../assets/icons/person";
+
 const Blog = () => {
+  const featureData = [
+    {
+      id: 1,
+      name: "Expert Team",
+      img: `${groupImg}`,
+    },
+    {
+      id: 2,
+      name: "Timely Delivery",
+      img: `${deliveryImg}`,
+    },
+    {
+      id: 3,
+      name: "24/7 Support",
+      img: `${personImg}`,
+    },
+    {
+      id: 4,
+      name: "Best Equipment",
+      img: `${wrenchImg}`,
+    },
+    {
+      id: 5,
+      name: "100% Guranty",
+      img: `${checkImg}`,
+    },
+    {
+      id: 6,
+      name: "Timely Delivery",
+      img: `${deliveryImg}`,
+    },
+  ];
+
   return (
-    <div>
-      <div className="text-center">
-        <h3 className="text-red-400 font-bold">Team</h3>
-        <h1 className="text-3xl font-bold">Meet Our Team</h1>
-        <p className="">
-          the majority have suffered alteration in some form, by injected
-          humour, or randomised words which don't look even slightly believable.
-        </p>
+    <div className="mt-10">
+      <div className="flex justify-center">
+        <div className="w-[70%] text-center space-y-2">
+          <h3 className="text-red-400 font-bold">Core Features</h3>
+          <h1 className="text-3xl font-bold">Why Choose Us</h1>
+          <p className="">
+            The majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable.
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center">
+      <div className="grid grid-cols-6 gap-4 mt-10 ">
+        {featureData.map((item) => {
+          return (
+            <div
+              key={item.id}
+              className="border border-gray-400 w-[170px] h-[156px] rounded-lg flex flex-col items-center justify-center"
+            >
+              <div>
+                <img src={item.img} alt="" />
+              </div>
+              <div>
+                <p className="">{item.name}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
       </div>
     </div>
   );
