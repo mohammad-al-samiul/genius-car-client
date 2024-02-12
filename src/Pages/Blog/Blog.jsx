0a79447603289e0d44248747bdc63460e0e87c0e
Noob-H4cker <alsamiul123@gmail.com>
2024-02-12 20:47:40 +0600

    blog section responsive

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -41,35 +41,35 @@ const Blog = () => {
 
   return (
     <div className="mt-10">
-      <div className="flex justify-center">
-        <div className="w-[70%] text-center space-y-2">
+      <div className="lg:flex justify-center">
+        <div className="px-4 lg:w-[70%] text-center space-y-2">
           <h3 className="text-red-400 font-bold">Core Features</h3>
           <h1 className="text-3xl font-bold">Why Choose Us</h1>
-          <p className="">
+          <p className="text-center">
             The majority have suffered alteration in some form, by injected
             humour, or randomised words which don't look even slightly
             believable.
           </p>
         </div>
       </div>
-      <div className="flex justify-center">
-      <div className="grid grid-cols-6 gap-4 mt-10 ">
-        {featureData.map((item) => {
-          return (
-            <div
-              key={item.id}
-              className="border border-gray-400 w-[170px] h-[156px] rounded-lg flex flex-col items-center justify-center"
-            >
-              <div>
-                <img src={item.img} alt="" />
+      <div className="lg:flex justify-center">
+        <div className="grid lg:grid-cols-6 gap-3 mt-10 grid-cols-2 mx-5">
+          {featureData.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="border border-gray-400 lg:w-[170px] lg:h-[156px] w-[150px] h-[135px] rounded-lg flex flex-col items-center justify-center"
+              >
+                <div>
+                  <img src={item.img} alt="" />
+                </div>
+                <div>
+                  <p className="">{item.name}</p>
+                </div>
               </div>
-              <div>
-                <p className="">{item.name}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
